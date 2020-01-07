6e829501c0f5d86ecb80d5bb472cde937a018717
unknown <artitnarasan@hotmail.co.th>
2020-01-07 14:00:47 +0700

    projectlogin

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -12,12 +12,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'HOME',
       url: '/home',
       icon: 'home'
     },
     {
-      title: 'List',
+      title: 'LIST',
       url: '/list',
       icon: 'list'
     }
